530f5aac7a14ca77741885f9e37a9249817c3c86
syamasaw <syamasaw@student.42tokyo.jp>
2024-02-28 19:50:06 +0900

    divide tokens for lexer

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: syamasaw <syamasaw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 17:52:14 by syamasaw          #+#    #+#             */
-/*   Updated: 2024/02/26 18:32:19 by syamasaw         ###   ########.fr       */
+/*   Updated: 2024/02/28 18:07:27 by syamasaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,10 @@
 # include <readline/readline.h>
 
 # include <termios.h>
+
+size_t	ft_strlcpy(char *restrict dest, const char *restrict src, size_t size);
+size_t	ft_strlen(const char *str);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strchr(const char *s, int c);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,11 +6,18 @@
 /*   By: syamasaw <syamasaw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 17:08:00 by syamasaw          #+#    #+#             */
-/*   Updated: 2024/02/28 14:23:06 by syamasaw         ###   ########.fr       */
+/*   Updated: 2024/02/28 19:27:16 by syamasaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+
+// typedef struct s_token
+// {
+// 	t_token	*next;
+// 	t_token	*prev;
+// 	char	*token_str;
+// }			t_token;
 
 void	ft_echo(char *line)
 {
@@ -35,6 +42,42 @@ void	sig_handler(int signal)
 		rl_replace_line("", 0);
 		rl_redisplay();
 	}
+}
+
+int	lexer(char *line) //lineはreadlineの出力
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	while (line[i] != '\0')
+	{
+		str = NULL;
+		if (ft_strchr("<>|", line[i]) != NULL)
+		{
+			j = 1;
+			if (line[i] != '|' && line[i] == line[i + 1])
+				j = 2;
+			str = ft_substr(line, i, j);
+			printf("%s\n", str);
+			free(str);
+			i += j;
+		}
+		else if (line[i] != ' ')
+		{
+			j = 0;
+			while (line[j + i] != '\0' && ft_strchr(" <>|", line[j + i]) == NULL)
+				j++;
+			str = ft_substr(line, i, j);
+			printf("%s\n", str);
+			free(str);
+			i += j;
+		}
+		if (line[i] == ' ')
+			i++;
+	}
+	return (0);
 }
 
 int	main(int argc, char *argv[], char *envp[])
@@ -71,6 +114,7 @@ int	main(int argc, char *argv[], char *envp[])
 		}
 		else if (strncmp(line, "echo", 4) == 0)
 			ft_echo(line);
+		lexer(line);
 		add_history(line);
 		free(line);
 	}
