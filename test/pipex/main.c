40b41bea638cd829f1d508a92c731153181858f9
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2024-03-02 17:46:33 +0000

    Update main.c file in test/pipex directory

--- test/pipex/main.c
+++ test/pipex/main.c
@@ -6,7 +6,7 @@
 /*   By: saraki <saraki@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 15:41:53 by syamasaw          #+#    #+#             */
-/*   Updated: 2024/03/01 17:30:32 by saraki           ###   ########.fr       */
+/*   Updated: 2024/03/02 15:49:08 by saraki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,3 +58,44 @@ static void	open_io_files(int argc, char *argv[], t_pipex *pipex)
 	if (argc != 5 || ft_strlen(argv[2]) <= 0 || ft_strlen(argv[3]) <= 0)
 		exit(close_fd(pipex->io_fds, 1));
 }
+
+// int	main(int argc, char *argv[])
+// {
+// 	int pipefd[2];
+// 	pid_t cpid;
+// 	char buf;
+
+// 	if (argc != 2) {
+// 		fprintf(stderr, "Usage: %s <string>\n", argv[0]);
+// 		exit(EXIT_FAILURE);
+// 	}
+
+// 	if (pipe(pipefd) == -1) {
+// 		perror("pipe");
+// 		exit(EXIT_FAILURE);
+// 	}
+
+// 	cpid = fork();
+// 	if (cpid == -1) {
+// 		perror("fork");
+// 		exit(EXIT_FAILURE);
+// 	}
+
+// 	if (cpid == 0) {    /* Child reads from pipe */
+// 		close(pipefd[1]);          /* Close unused write end */
+
+// 		while (read(pipefd[0], &buf, 1) > 0)
+// 			write(STDOUT_FILENO, &buf, 1);
+
+// 		write(STDOUT_FILENO, "\n", 1);
+// 		close(pipefd[0]);
+// 		_exit(EXIT_SUCCESS);
+
+// 	} else {            /* Parent writes argv[1] to pipe */
+// 		close(pipefd[0]);          /* Close unused read end */
+// 		write(pipefd[1], argv[1], strlen(argv[1]));
+// 		close(pipefd[1]);          /* Reader will see EOF */
+// 		wait(NULL);                /* Wait for child */
+// 		exit(EXIT_SUCCESS);
+// 	}
+// }
