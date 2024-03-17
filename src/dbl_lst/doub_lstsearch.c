8366f7f5d4483ab8d46aefe150e5ef7b121f40f1
GawinGowin <awmgdgdm@gmail.com>
2024-03-18 02:50:16 +0900

    add doub_lstsearch

---
+++ src/dbl_lst/doub_lstsearch.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   doub_lstsearch.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saraki <saraki@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/19 18:34:48 by saraki            #+#    #+#             */
+/*   Updated: 2024/03/18 02:45:46 by saraki           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+typedef int (t_cmp_f)(void *, void *);
+
+t_dbl_list	*doub_lstsearch(t_dbl_list *lst, void *query_pt, t_cmp_f cmp_f)
+{
+	t_dbl_list	*ret_node;
+	int		i;
+
+	if (lst == NULL)
+		return (NULL);
+	i = 0;
+	ret_node = lst;
+	while (ret_node->data != NULL)
+	{
+		if (cmp_f(ret_node->data, query_pt))
+			return (ret_node);
+		ret_node = ret_node->next;
+		i ++;
+	}
+	return (ret_node);
+}
