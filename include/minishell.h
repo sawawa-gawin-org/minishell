8e9ca4e8d5a2494174ef26387b35a176799010a0
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2024-03-28 16:04:01 +0000

    resolve norm err

--- include/minishell.h
+++ include/minishell.h
@@ -39,25 +39,33 @@ typedef enum e_token_type
 	open_quote
 }	t_token_type;
 
-typedef enum e_tokens {
-    TOKEN_FLAG = 1,
-    TUBE_FLAG = 1 << 1,
-    LESS_FLAG = 1 << 2,
-    GREAT_FLAG = 1 << 3,
-    HEREDOC_FLAG = 1 << 4,
-    APPEND_FLAG = 1 << 5,
-    DOUBLE_QUOTE_FLAG = 1 << 6,
-    DOUBLE_QUOTE_VAL_FLAG = 1 << 7,
-    SINGLE_QUOTE_FLAG = 1 << 8,
-    VAL_FLAG = 1 << 9,
-    SPACE_FLAG = 1 << 10,
-    OPEN_QUOTE_FLAG = 1 << 11
-} t_tokens;
+typedef enum e_tokens
+{
+	TOKEN_FLAG = 1,
+	TUBE_FLAG = 1 << 1,
+	LESS_FLAG = 1 << 2,
+	GREAT_FLAG = 1 << 3,
+	HEREDOC_FLAG = 1 << 4,
+	APPEND_FLAG = 1 << 5,
+	DOUBLE_QUOTE_FLAG = 1 << 6,
+	DOUBLE_QUOTE_VAL_FLAG = 1 << 7,
+	SINGLE_QUOTE_FLAG = 1 << 8,
+	VAL_FLAG = 1 << 9,
+	SPACE_FLAG = 1 << 10,
+	OPEN_QUOTE_FLAG = 1 << 11
+}	t_tokens;
 
-# define META_FLAG (LESS_FLAG | GREAT_FLAG | TUBE_FLAG | HEREDOC_FLAG | APPEND_FLAG)
-# define QUOTE_FLAG (OPEN_QUOTE_FLAG | SINGLE_QUOTE_FLAG | DOUBLE_QUOTE_VAL_FLAG | DOUBLE_QUOTE_FLAG)
-# define WORD_FLAG (VAL_FLAG | TOKEN_FLAG)
-# define BLANK_FLAG (SPACE_FLAG)
+// # define META_FLAG (LESS_FLAG | GREAT_FLAG | TUBE_FLAG | \
+// 					HEREDOC_FLAG | APPEND_FLAG)
+// # define QUOTE_FLAG (OPEN_QUOTE_FLAG | SINGLE_QUOTE_FLAG | \
+// 					DOUBLE_QUOTE_VAL_FLAG | DOUBLE_QUOTE_FLAG)
+// # define WORD_FLAG (VAL_FLAG | TOKEN_FLAG)
+// # define BLANK_FLAG (SPACE_FLAG)
+
+# define META_FLAG 62
+# define QUOTE_FLAG 2496
+# define WORD_FLAG 513
+# define BLANK_FLAG 1024
 
 typedef struct s_token
 {
@@ -67,13 +75,13 @@ typedef struct s_token
 	char			*token_str;
 }					t_token;
 
-typedef struct		s_token_data
+typedef struct s_token_data
 {
 	t_tokens		token_type;
 	char			*token_str;
 }					t_token_data;
 
-typedef struct	s_shval_data
+typedef struct s_shval_data
 {
 	char			*key;
 	char			*val;
@@ -103,7 +111,6 @@ t_token	*lst_last(t_token *tokens);
 t_token	*lst_new(void);
 t_token	*lstadd_token(t_token *tokens, char *str, int type);
 
-
 // parser
 int		syntax_checker(t_token *tokens);
 
@@ -114,8 +121,8 @@ void	del_lst(t_token *tokens);
 void	*get_env_all(char **envp);
 void	free_shval_data(void *data);
 
-int	parser(t_token **tokens);
+int		parser(t_token **tokens);
 
-int	minishell(int argc, char *argv[], char *envp[]);
+int		minishell(int argc, char *argv[], char *envp[]);
 
 #endif
