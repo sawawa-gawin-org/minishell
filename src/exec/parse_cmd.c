6a8d7e6317d05a03352c741f0d15beb1489f0476
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2024-05-15 16:09:24 +0000

    avoid unused err

--- src/exec/parse_cmd.c
+++ src/exec/parse_cmd.c
@@ -6,7 +6,7 @@
 /*   By: saraki <saraki@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 07:27:21 by saraki            #+#    #+#             */
-/*   Updated: 2024/05/15 08:34:18 by saraki           ###   ########.fr       */
+/*   Updated: 2024/05/15 16:04:29 by saraki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ char	**parse_cmd(t_tokenlst *head_node, t_pipex *pipe)
 	cmd = convert_tokenlst_to_char_array(head_node);
 	if (cmd == NULL)
 		return (NULL);
+	(void) err;
 	return (cmd);
 }
 
