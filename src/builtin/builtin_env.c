d17d39d9e7eb79b62df24308f901db317b4b2168
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2024-08-01 14:31:02 +0000

    chore: norminette対応

--- src/builtin/builtin_env.c
+++ src/builtin/builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: saraki <saraki@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 13:30:34 by saraki            #+#    #+#             */
-/*   Updated: 2024/08/01 14:22:34 by saraki           ###   ########.fr       */
+/*   Updated: 2024/08/01 14:29:46 by saraki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,14 @@ static int	valid_option(char **cmd);
 /**
  * Executes the built-in command "env".
  *
- * This function takes an array of command arguments `cmd` and a pointer to a linked list `envlst`
- * representing the environment variables. It executes the "env" command, which prints the current
- * environment variables to the standard output.
+ * This function takes an array of command arguments `cmd` and a pointer to a
+ * linked list `envlst` representing the environment variables. It executes the
+ * "env" command, which prints the current environment variables to the standard
+ * output.
  *
  * @param cmd     The array of command arguments.
- * @param envlst  A pointer to the linked list representing the environment variables.
+ * @param envlst  A pointer to the linked list representing the environment
+ * variables.
  * @return        Returns 0 on success, or a non-zero value on failure.
  */
 int	builtin_env(char **cmd, t_blst **envlst, int mode)
