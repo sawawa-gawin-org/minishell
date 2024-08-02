e4aff3f7e9d484a7bc41f59709cf2964b2a83136
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2024-08-02 07:46:31 +0000

    chore: Add "libft.h" header to minishell.c

--- cmd/minishell/minishell.c
+++ cmd/minishell/minishell.c
@@ -6,7 +6,7 @@
 /*   By: saraki <saraki@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 17:08:00 by syamasaw          #+#    #+#             */
-/*   Updated: 2024/07/21 07:23:09 by saraki           ###   ########.fr       */
+/*   Updated: 2024/08/02 05:32:00 by saraki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include "env.h"
 #include "tokens.h"
 #include "dbllst.h"
+#include "libft.h"
 
 // void	end(void)__attribute__((destructor));
 
@@ -61,7 +62,10 @@ static int	main_loop(void *env_lst)
 	if (g_signal != 0)
 		g_signal = 0;
 	if (line == NULL)
+	{
+		ft_putstr_fd("exit\n", 2);
 		return (ERR);
+	}
 	if (is_blank_str(line))
 	{
 		free(line);
