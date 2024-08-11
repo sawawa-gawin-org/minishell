25c8a7e02a19630780be82d93d6ad609a1c5a64b
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2024-08-11 19:35:24 +0900

    fix: Segfaultの原因を修正 (#125)

--- src/exec/exec_int.h
+++ src/exec/exec_int.h
@@ -6,7 +6,7 @@
 /*   By: saraki <saraki@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 17:23:14 by saraki            #+#    #+#             */
-/*   Updated: 2024/08/01 18:38:00 by saraki           ###   ########.fr       */
+/*   Updated: 2024/08/10 10:18:43 by saraki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,17 +52,16 @@ int				exec(
 					t_tokenlst **token_head_node,
 					char **env,
 					t_blst **env_lst);
-t_tokenlst		*shift_token_section(t_tokenlst *token_head_node, int index);
 
 int				make_processes(t_exec_parametors *param);
 int				make_process(t_exec_parametors *param, t_callback callback);
 
-char			**parse_cmd(t_tokenlst **head_node, t_pipex *pipe);
+char			**parse_cmd(t_tokenlst *token_list, t_pipex *pipe);
 
 char			**convert_tokenlst_to_char_array(
 					t_tokenlst *section_start_node);
 char			*find_cmd(char *path, char **env, int *status);
-void			parse_redirects(t_tokenlst **now_node, t_pipex *pipe);
+void			parse_redirects(t_tokenlst *now_node, t_pipex *pipe);
 void			do_first_process(t_callback_parametors *params);
 void			do_middle_process(t_callback_parametors *params);
 void			do_last_process(t_callback_parametors *params);
