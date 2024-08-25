af417d850a12b93c8ad1a05b89d5abaf49f21eb9
GawinGowin <saraki@student.42tokyo.jp>
2024-08-25 18:45:44 +0900

    fix(exec): パース後のコマンドが空の場合に正常終了するようにした

--- src/builtin/core.c
+++ src/builtin/core.c
@@ -6,7 +6,7 @@
 /*   By: saraki <saraki@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 03:56:27 by syamasaw          #+#    #+#             */
-/*   Updated: 2024/08/17 17:32:28 by saraki           ###   ########.fr       */
+/*   Updated: 2024/08/25 18:43:26 by saraki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,6 @@
  */
 int	is_builtin(char *cmd)
 {
-	if (cmd == NULL)
-		return (0);
 	if (ft_strcmp(cmd, "echo") == 0)
 		return (1);
 	if (ft_strcmp(cmd, "cd") == 0)
--- src/exec/make_processes_util.c
+++ src/exec/make_processes_util.c
@@ -6,7 +6,7 @@
 /*   By: saraki <saraki@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 15:25:30 by saraki            #+#    #+#             */
-/*   Updated: 2024/08/19 04:39:54 by saraki           ###   ########.fr       */
+/*   Updated: 2024/08/25 18:42:29 by saraki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,8 @@ int	make_process(t_exec_parametors *param, t_callback callback)
 	callback_args.path = NULL;
 	callback_args.env = param->env;
 	callback_args.env_lst = param->env_lst;
+	if (*(callback_args.cmd) == NULL)
+		return (OK);
 	if (!is_builtin(callback_args.cmd[0]))
 	{
 		callback_args.path = find_cmd(callback_args.cmd[0], param->env,
